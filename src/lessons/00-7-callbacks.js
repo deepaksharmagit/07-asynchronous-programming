0239c7000834ab604d602e69727e32a7412b2460
Keegan Leary <35752455+learyjk@users.noreply.github.com>
2024-01-25 15:59:28 -0800

    solutions

---
+++ src/lessons/00-7-callbacks.js
@@ -0,0 +1,13 @@
+// Task 1
+function customSetTimeout(callback, delay) {
+  // Pass the callback function and delay to the standard setTimeout function
+  setTimeout(callback, delay);
+}
+
+function delayedMessage() {
+  console.log("This message is delayed by 1 seconds.");
+}
+
+// Task 2
+// Call customeSetTimeout in a way that executes the console.log in delayedMessage after 1 second (1000 ms)
+customSetTimeout(delayedMessage, 1000);
