9fc605b4a80e5e515581f5ef9b54cce7299cc20d
Keegan Leary <35752455+learyjk@users.noreply.github.com>
2024-01-26 13:36:46 -0800

    merge from solutions again

--- src/lessons/00-7-callbacks.js
+++ src/lessons/00-7-callbacks.js
@@ -1,9 +1,6 @@
 function customSetTimeout(callback, delay) {
   // Pass the callback function and delay to the standard setTimeout function
-<<<<<<< HEAD
-=======
   setTimeout(callback, delay);
->>>>>>> solutions
 }
 
 function delayedMessage() {
@@ -11,7 +8,4 @@ function delayedMessage() {
 }
 
 // Call customeSetTimeout in a way that executes the console.log in delayedMessage after 1 second (1000 ms)
-<<<<<<< HEAD
-=======
 customSetTimeout(delayedMessage, 1000);
->>>>>>> solutions
--- src/lessons/01-7-callback-hell.js
+++ src/lessons/01-7-callback-hell.js
@@ -1,36 +1,23 @@
 function fetchData(callback) {
   setTimeout(function () {
-<<<<<<< HEAD
-    console.log("Data fetched!");
-    callback();
-  }, 2000);
-=======
     // Log "Data fetched!" to the console
     console.log("Data fetched!");
     // Invoke the callback function
     callback();
   }, 1000);
->>>>>>> solutions
 }
 
 function processData(callback) {
   setTimeout(function () {
     console.log("Data processed!");
     callback();
-<<<<<<< HEAD
-  }, 1500);
-=======
   }, 500);
->>>>>>> solutions
 }
 
 function displayData() {
   console.log("Data displayed!");
 }
 
-<<<<<<< HEAD
-// Usage of callback hell
-=======
 /**
  * Use the function above to log the following in otder to the console:
  *
@@ -42,35 +29,8 @@ function displayData() {
  * https://dev.to/jerrycode06/callback-hell-and-how-to-rescue-it-ggj
  *
  *  */
->>>>>>> solutions
 fetchData(function () {
   processData(function () {
     displayData();
   });
 });
-<<<<<<< HEAD
-
-// Task 2 (Using Promises)
-function fetchDataWithPromise() {
-  return new Promise(function (resolve, reject) {
-    setTimeout(function () {
-      console.log("Data fetched!");
-      resolve();
-    }, 2000);
-  });
-}
-
-fetchDataWithPromise()
-  .then(function () {
-    return new Promise(function (resolve, reject) {
-      setTimeout(function () {
-        console.log("Data processed!");
-        resolve();
-      }, 1500);
-    });
-  })
-  .then(function () {
-    console.log("Data displayed!");
-  });
-=======
->>>>>>> solutions
