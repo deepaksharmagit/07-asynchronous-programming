9c833524486ce4d66318c33e1cefb03ff68fef6e
Keegan Leary <35752455+learyjk@users.noreply.github.com>
2024-01-26 17:25:42 -0800

    main ready

--- src/webflow/index.js
+++ src/webflow/index.js
@@ -1,40 +1,6 @@
-import { postNewQuote } from "./postNewQuote.js";
-import { fetchQuotes } from "./fetchQuotes.js";
-
-async function init() {
-  getAndDisplayNewQuote();
-
-  const getQuoteButton = document.querySelector("#get-quote-button");
-  getQuoteButton.addEventListener("click", async () => {
-    getAndDisplayNewQuote();
-  });
-
-  const addQuoteForm = document.querySelector("#add-quote-form");
-  addQuoteForm.addEventListener("submit", async (event) => {
-    console.log("Form submitted!");
-    event.preventDefault();
-    event.stopPropagation();
-    await addQuote();
-  });
-}
+async function init() {}
 init();
 
-async function getAndDisplayNewQuote() {
-  try {
-    const data = await fetchQuotes();
-    console.log(data);
-    const quoteTextElement = document.querySelector(".quote-text");
-    const random = Math.floor(Math.random() * data.length);
-    quoteTextElement.textContent = data[random].quote;
-  } catch (error) {
-    console.error("Error:", error);
-  }
-}
+async function getAndDisplayNewQuote() {}
 
-async function addQuote() {
-  const quoteInputElement = document.querySelector("#add-quote-input");
-  const quote = quoteInputElement.value;
-  const data = await postNewQuote(quote);
-  console.log(data);
-  quoteInputElement.value = "";
-}
+async function addQuote() {}
